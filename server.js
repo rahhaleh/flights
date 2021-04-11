a2376eaefe55f2b4db29e0f9c56a8521acf06426
Nizar Alsaeed <nizar.ayman@gmail.com>
2021-04-11 21:15:33 +0300

    done airline ratings

--- server.js
+++ server.js
@@ -37,8 +37,39 @@ this.airline=(data.airline.name)?data.airline.name:'no available airline name';
 this.flight=(data.flight.number)?data.flight.number:'no available flight number';
 }
 
+// 1- insert all the NEW airlines names to the airlines table getting them from the flights_info table in the airline column.
+// 2- select all unique airline names from table airlines to loop throw them.
+// 3- loop throw the airline names and calculate the average using sql AVG function and push them into ratesArr.
+// 4- combine the airlinesNamesArr and ratesArr into one array of objects.
+// 5- sort the array of objects and then render the page.
 function renderHomePage(request,response){
-    response.render('./')
+    let combinedArrOfObj=[];
+    let sql;
+    sql = `INSERT INTO airlines (airline) SELECT airline FROM flights_info WHERE flights_info.airline NOT in (SELECT airline FROM airlines)`;
+    client.query(sql).then(()=>{
+        sql = `SELECT DISTINCT airline FROM airlines`;
+        client.query(sql).then(result=>{
+            let airlinesNamesArr= result.rows.map(obj=>Object.values(obj)[0]);
+            sql = `SELECT AVG(flight_rate) FROM reviews WHERE flight_id in (select id from flights_info where airline = $1)`;
+            let ratesArr=[];
+            for (let i = 0; i < airlinesNamesArr.length; i++) {
+                let values=[airlinesNamesArr[i]];
+                ratingPromis=client.query(sql,values).then(result=>{
+                    ratesArr.push(parseFloat(result.rows[0].avg).toFixed(2));
+                });             
+            }
+            ratingPromis.then(()=>{
+                for (let j = 0; j < airlinesNamesArr.length; j++) {
+                    combinedArrOfObj[j]={[airlinesNamesArr[j]]:ratesArr[j]};
+                }
+                //sorting the combinedArrOfObj from hieght rate to lowest.
+                combinedArrOfObj.sort((a,b)=>Object.values(b)[0]-Object.values(a)[0]);
+                response.render('./',{result:combinedArrOfObj});
+                console.log('combinedArrOfObj', combinedArrOfObj);
+            });
+        });
+    });
+    
 }
 
 function getData(request,response){
@@ -130,9 +161,6 @@ function renderCommunity(request,response){
 
     });
 }
-
-
-
 
 client.connect(()=>{
     app.listen(PORT, () => {console.log(`Listening to Port ${PORT}`);});
